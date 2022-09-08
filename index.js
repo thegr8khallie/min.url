af59fd0ae0279ab509f913b197c0f990bc576c13
Victor Anyadioha <talk2okwundu@gmail.com>
2022-09-08 14:49:52 +0100

    Completed NavBar: Styling and Responsiveness.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-import { landingPage } from "./scripts/landingPage.js";
-
-document.body.innerHTML = landingPage;
+const ham = document.querySelector(".hamburger-wrapper");
+const hamNav = document.querySelector(".navi-element");
+const hamAnime = () => {
+  hamNav.classList.toggle("ham-menu");
+};
+ham.addEventListener("click", hamAnime);

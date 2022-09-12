3a440d38064e69633976827ca2e13c9d337dcac3
Victor Anyadioha <talk2okwundu@gmail.com>
2022-09-12 20:45:07 +0100

    Completed Hero and Form styles. Fine tuned navbar. To continue fron Info section intro

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+//Hamburger Scripts - start
 const ham = document.querySelector(".hamburger-wrapper");
 const hamNav = document.querySelector(".navi-element");
+
 const hamAnime = () => {
-  hamNav.classList.toggle("ham-menu");
+  if (hamNav.style.transform === "translate(-0.5rem, -20rem)") {
+    hamNav.style.transform = "translate(-0.5rem, 0.5rem)";
+  } else {
+    hamNav.style.transform = "translate(-0.5rem, -20rem)";
+  }
 };
 ham.addEventListener("click", hamAnime);
+//Hamburger Scripts - End
